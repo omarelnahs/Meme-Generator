334a839fe4ef98e8df7988fbd68fd6164671a1ed
omarelnahs <omarelnahas1141@email.com>
2023-09-08 11:18:54 +0300

    s

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,5 +1,4 @@
 import React from "react"
-import memesData from "../memesData.js"
 
 export default function Meme() {
     
@@ -8,13 +7,18 @@ export default function Meme() {
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    const [allMemes, setAllMemes] = React.useState([])
     
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => setAllMemes(data.data.memes))
+    }, [])
+
     
     function getMemeImage() {
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
@@ -32,7 +36,7 @@ export default function Meme() {
     
     return (
         <main>
-
+            
             <div className="form">
                 <input 
                     type="text"
